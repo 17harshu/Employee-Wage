b8e3a8c86e7d37ec60effb089ad0ae7d20f483e2
17harshu <harshalanandas17@gmail.com>
2022-02-07 15:03:12 +0530

    added:employee is present/absent

--- WelcomeEmployee.java
+++ WelcomeEmployee.java
@@ -1,7 +1,19 @@
 package com.employee.problems;
 
 public class WelcomeEmployee {
+	static int IS_FULL_TIME = 1;
+	
+	public static void empPresentAbsent() {
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+	}
+	
 	public static void main(String[] args) {
+		
 		System.out.println("Welcome to Employee Wage Computation Program");
+		empPresentAbsent();
 	}
 }
