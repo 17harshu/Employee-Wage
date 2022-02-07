512e4da2ee9ddcbe48df194c97f8cc746c9d69ff
17harshu <harshalanandas17@gmail.com>
2022-02-07 16:43:09 +0530

    added: solving using switch case

--- WelcomeEmployee.java
+++ WelcomeEmployee.java
@@ -5,6 +5,12 @@ public class WelcomeEmployee {
 	static int wagePerHour = 20;
 	static int partTimeHour = 8;
 	static int fullDayHour = 8;
+	int isPartTime = 1;
+	int isFullTime = 2;
+	int empRatePerHour = 20;
+	static int numWorkingDays = 20;
+	static int workingHours = 0;
+	static int totalSalary = 0;
 	
 	public static void empPresentAbsent() {
 		double empCheck = Math.floor(Math.random() * 10) % 2;
@@ -25,11 +31,33 @@ public class WelcomeEmployee {
 		System.out.println("Daily Employee Wage for Full Time is: " + empWage);
 	}
 	
+	public static void empWageUsingCase() {
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch(empCheck) {
+		case 1:
+			int halfWorkingHours=4;
+			int empSalary = halfWorkingHours * wagePerHour;
+			System.out.println("Employee Salary for Part Time is: " + empSalary);
+			break;
+		case 2:
+			int fullWorkingHours=8;
+			int Salary = fullWorkingHours * wagePerHour;
+			System.out.println("Employee Salary for Full Time is: " + Salary);
+			break;
+		default:
+			System.out.println("Employee is absent");
+		}
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation Program");
 		empPresentAbsent();
 		empWage();
 		empFullTime();
+		empWageUsingCase();
+		//wageForMonth();
 	}
 }
